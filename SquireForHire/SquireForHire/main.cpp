e82aaf7fdcf499274a9ea61bf753b5ab215f6482
ElanaBanana <s191591@students.aie.edu.au>
2024-02-23 16:55:12 +1030

    Initial commit

---
+++ SquireForHire/SquireForHire/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+Elana Parnis
+23/02/2024
+*/
+
+int main() {
+	//run game
+	//https://patorjk.com/software/taag/#p=display&f=Ogre&t=Squire%20for%20Hire Orge Font
+	cout << R"(
+	 __             _             __                     _          
+	/ _\ __ _ _   _(_)_ __ ___   / _| ___  _ __    /\  /(_)_ __ ___ 
+	\ \ / _` | | | | | '__/ _ \ | |_ / _ \| '__|  / /_/ / | '__/ _ \
+	_\ \ (_| | |_| | | | |  __/ |  _| (_) | |    / __  /| | | |  __/
+	\__/\__, |\__,_|_|_|  \___| |_|  \___/|_|    \/ /_/ |_|_|  \___|
+	       |_|                                                      
+)";
+
+	cout << R"(
++========+		
+[  PLAY  ]     
++========+		
+)";
+                                                     
+	//end game
+}
